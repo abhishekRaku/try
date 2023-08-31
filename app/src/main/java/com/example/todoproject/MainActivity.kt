42a5ffb3ac71d8ab54e24ddb199473b09aee4675
abhishek.abhishek <abhishek.abhishek@rakuten.com>
2023-08-31 15:57:21 +0900

    first

--- app/src/main/java/com/example/todoproject/MainActivity.kt
+++ app/src/main/java/com/example/todoproject/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
          viewModel = ViewModelProvider(this).get(NodeListViewModel::class.java)
 
         recyclerView = findViewById(R.id.recyclerView)
